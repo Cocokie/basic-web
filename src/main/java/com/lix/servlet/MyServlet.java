7fa41f5f9a337db73077f9d2727b52f872402221
lixin <1254759600@qq.com>
2020-03-20 00:25:41 +0800

    add

---
+++ src/main/java/com/lix/servlet/MyServlet.java
@@ -0,0 +1,29 @@
+package com.lix.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("你是神魔恋 ");
+        resp.getWriter().println("qxxxc");
+        ServletConfig servletConfig = getServletConfig();
+        System.out.println("============");
+        System.out.println(servletConfig.getInitParameter("name"));
+        System.out.println(servletConfig.getServletName());
+        ServletContext servletContext = servletConfig.getServletContext();
+        System.out.println(servletContext.getInitParameter("name"));
+      //  System.out.println(servletContext.getAttribute("name"));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}

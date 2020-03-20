e11bcae7d6b43c409c8114ccb7ffa34aa603c484
lixin <1254759600@qq.com>
2020-03-20 17:36:23 +0800

    add

--- src/main/java/com/lix/servlet/MyServlet.java
+++ src/main/java/com/lix/servlet/MyServlet.java
@@ -1,8 +1,6 @@
 package com.lix.servlet;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,15 +9,18 @@ import java.io.IOException;
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("你是神魔恋 ");
-        resp.getWriter().println("qxxxc");
-        ServletConfig servletConfig = getServletConfig();
-        System.out.println("============");
-        System.out.println(servletConfig.getInitParameter("name"));
-        System.out.println(servletConfig.getServletName());
-        ServletContext servletContext = servletConfig.getServletContext();
-        System.out.println(servletContext.getInitParameter("name"));
-      //  System.out.println(servletContext.getAttribute("name"));
+
+//        System.out.println("你是神魔恋");
+//        resp.getWriter().println("qq");
+//        ServletConfig servletConfig = getServletConfig();
+//
+//        System.out.println("============");
+//        System.out.println(servletConfig.getInitParameter("name"));
+//        System.out.println(servletConfig.getServletName());
+//        ServletContext servletContext = servletConfig.getServletContext();
+//        System.out.println(servletContext.getInitParameter("name"));
+        System.out.println("servlet");
+        //  System.out.println(servletContext.getAttribute("name"));
     }
 
     @Override
